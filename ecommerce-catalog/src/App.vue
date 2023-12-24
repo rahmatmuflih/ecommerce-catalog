a803f4dd46649b85d23b35015f865da3630c56b2
Muflih <mufliharifinrahmat@gmail.com>
2023-12-24 19:28:55 +0700

    update project

--- ecommerce-catalog/src/App.vue
+++ ecommerce-catalog/src/App.vue
@@ -1,47 +1,29 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  name: "Hello World",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      foods: ["Pizza", "French Fries", "Hotdog", "Tacos", "Spaghetti"],
+    };
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <ul v-for="food in foods" v-bind:key="food">
+      {{
+        food
+      }}
+    </ul>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+h1 {
+  color: red;
 }
 </style>
