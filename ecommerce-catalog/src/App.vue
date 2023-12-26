d6cc71bc51bb9bf19c7b6e9f49d250817804c026
Muflih <mufliharifinrahmat@gmail.com>
2023-12-26 07:03:53 +0700

    update

--- ecommerce-catalog/src/App.vue
+++ ecommerce-catalog/src/App.vue
@@ -1,29 +1,78 @@
 <script>
 export default {
-  name: "Hello World",
-  props: {
-    msg: String,
-  },
   data() {
     return {
-      foods: ["Pizza", "French Fries", "Hotdog", "Tacos", "Spaghetti"],
+      products: [],
     };
+  },
+  computed: {
+    productCategory() {
+      if (this.products.category === "men's clothing") {
+        return "men-clothing";
+      } else if (this.products.category === "women's clothing") {
+        return "women-clothing";
+      } else {
+        return "unavailable";
+      }
+    },
+  },
+  mounted() {
+    this.fetchProduct();
+  },
+  methods: {
+    getRating() {
+      if (this.products.rating) {
+        return this.products.rating.rate;
+      } else {
+        return "N/A";
+      }
+    },
+    async fetchProduct() {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products/1");
+        const data = await response.json();
+        this.products = data;
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    },
   },
 };
 </script>
 
 <template>
-  <main>
-    <ul v-for="food in foods" v-bind:key="food">
-      {{
-        food
-      }}
-    </ul>
-  </main>
+  <div :class="productCategory">
+    <div class="hero"><img src="./assets/img/bg-pattern.svg" alt="" /></div>
+    <div class="product">
+      <div class="img-prod">
+        <img :src="products.image" alt="" height="480px" width="350px" />
+      </div>
+      <div class="detail-prod">
+        <h1>
+          {{ products.title }}
+        </h1>
+        <div class="cat-rat">
+          <p>
+            {{ products.category }}
+          </p>
+          <p>
+            {{ getRating() }}
+            /5 <span class="stars"></span>
+          </p>
+        </div>
+        <hr />
+        <p class="desc">
+          {{ products.description }}
+        </p>
+        <hr />
+        <p>${{ products.price }}</p>
+        <div class="buy">Buy</div>
+        <div class="next">Next Product</div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-h1 {
-  color: red;
-}
+<style>
+@import "./assets/css/base.css";
 </style>
